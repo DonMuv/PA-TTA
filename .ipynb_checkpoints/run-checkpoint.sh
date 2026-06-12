83c26d447b1ecfb37fa19e753aafd7384a580cb0
DonMuv <46625405+DonMuv@users.noreply.github.com>
2026-06-12 17:32:12 +0800

    All good

--- .ipynb_checkpoints/run-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-
-# CUDA_VISIBLE_DEVICES=0 python run.py --cfg ./configs/source.yaml
-# CUDA_VISIBLE_DEVICES=0 python run.py --cfg ./configs/cotta.yaml
-CUDA_VISIBLE_DEVICES=0 python run.py --cfg ./configs/ours.yaml
